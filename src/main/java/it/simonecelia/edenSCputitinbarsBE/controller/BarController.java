5364ec958fa2c9cc416adef38a9de237c33422c7
Simone Celia <simone.celia@simonecelia.it>
2024-12-22 20:22:47 +0100

    init

---
+++ src/main/java/it/simonecelia/edenSCputitinbarsBE/controller/BarController.java
@@ -0,0 +1,26 @@
+package it.simonecelia.edenSCputitinbarsBE.controller;
+
+import io.quarkus.logging.Log;
+import it.simonecelia.edenSCputitinbarsBE.service.BarService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+
+@Path ( "/bar" )
+public class BarController {
+
+	@Inject
+	BarService barService;
+
+	@POST
+	@Produces ( MediaType.APPLICATION_JSON )
+	public Response newBar () {
+		Log.info ( "Calling newBar" );
+		barService.newBar ();
+		return Response.status ( Response.Status.CREATED ).build ();
+	}
+}
