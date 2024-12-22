733e707947b9c1508a92cf4dd968e717363ebdee
Simone Celia <simone.celia@eng.it>
2024-12-22 20:22:47 +0100

    init

---
+++ src/main/java/it/simonecelia/edenSCputitinbarsBE/service/BarService.java
@@ -0,0 +1,11 @@
+package it.simonecelia.edenSCputitinbarsBE.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+
+@ApplicationScoped
+public class BarService {
+
+	public void newBar () {
+	}
+}
