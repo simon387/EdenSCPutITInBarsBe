b9663f4157b0237697125ffaf83ae2ac1aad43a0
Simone Celia <simone.celia@simonecelia.it>
2024-12-23 12:17:29 +0100

    riconcv

--- src/main/java/it/simonecelia/edenSCputitinbarsBE/service/BarService.java
+++ src/main/java/it/simonecelia/edenSCputitinbarsBE/service/BarService.java
@@ -1,12 +1,16 @@
 package it.simonecelia.edenSCputitinbarsBE.service;
 
-import it.simonecelia.edenSCputitinbarsBE.dto.ReportDTO;
+import it.simonecelia.edenSCputitinbarsBE.enumeration.Realm;
 import jakarta.enterprise.context.ApplicationScoped;
 
 
 @ApplicationScoped
 public class BarService {
 
-	public void newBar ( ReportDTO report ) {
+	String[] arrSkillsAlbName =  {"heated evocation sigil", "earthen fervor sigil", "heated battle jewel", "vapor war sigil", "fiery war sigil", "ashen fervor sigil", "vacuous fervor sigil", "icy war sigil", "earthen evocation sigil", "airy fervor sigil", "dusty battle jewel", "molten magma war sigil", "fiery evocation sigil", "watery fervor sigil", "icy evocation sigil", "vapor fervor sigil", "airy war sigil", "dusty evocation sigil", "watery evocation sigil", "salt encrusted fervor sigil", "vapor battle jewel", "earthen war sigil", "fiery battle jewel", "watery war sigil", "fiery fervor sigil", "steaming fervor sigil", "vapor evocation sigil", "earthen battle jewel",  "airy battle jewel", "dusty war sigil", "heated war sigil", "airy evocation sigil", "finesse war sigil", "finesse fervor sigil"};
+
+
+	public void newBar ( Realm realm, String character, String payload ) {
+
 	}
 }
