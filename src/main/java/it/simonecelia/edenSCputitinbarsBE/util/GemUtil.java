5afca4e7e9cb331e7e14861656afa85a2d16be0d
Simone Celia <simone.celia@eng.it>
2024-12-23 16:55:42 +0100

    data

--- src/main/java/it/simonecelia/edenSCputitinbarsBE/util/GemUtil.java
+++ src/main/java/it/simonecelia/edenSCputitinbarsBE/util/GemUtil.java
@@ -6,6 +6,14 @@ public class GemUtil {
 		return switch ( ( strength + " " + name ).toLowerCase () ) {
 			case "raw fiery essence jewel" -> 1300000;
 			case "uncut fiery essence jewel" -> 1300001;
+			case "rough fiery essence jewel" -> 1300002;
+			case "flawed fiery essence jewel" -> 1300003;
+			case "imperfect fiery essence jewel" -> 1300004;
+			case "polished fiery essence jewel" -> 1300005;
+			case "faceted fiery essence jewel" -> 1300006;
+			case "precious fiery essence jewel" -> 1300007;
+			case "flawless fiery essence jewel" -> 1300008;
+			case "perfect fiery essence jewel" -> 1300009;
 			default -> 0;
 		};
 	}
