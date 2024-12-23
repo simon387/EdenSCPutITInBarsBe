43ded6167f1841e0b6e507017ac841aebb0918a2
Simone Celia <simone.celia@eng.it>
2024-12-23 17:19:18 +0100

    data entry

--- src/main/java/it/simonecelia/edenSCputitinbarsBE/service/BarService.java
+++ src/main/java/it/simonecelia/edenSCputitinbarsBE/service/BarService.java
@@ -18,7 +18,7 @@ import java.util.stream.Stream;
 @ApplicationScoped
 public class BarService {
 
-	String[] arrSkillsAlbName =
+	private final String[] arrSkillsAlbName =
 					{ "heated evocation sigil", "earthen fervor sigil", "heated battle jewel", "vapor war sigil", "fiery war sigil", "ashen fervor sigil",
 									"vacuous fervor sigil", "icy war sigil", "earthen evocation sigil", "airy fervor sigil", "dusty battle jewel",
 									"molten magma war sigil", "fiery evocation sigil", "watery fervor sigil", "icy evocation sigil", "vapor fervor sigil",
@@ -27,13 +27,13 @@ public class BarService {
 									"vapor evocation sigil", "earthen battle jewel", "airy battle jewel", "dusty war sigil", "heated war sigil",
 									"airy evocation sigil", "finesse war sigil", "finesse fervor sigil" };
 
-	String[] arrSkillsMidName = { "airy chaos rune", "earthen war rune", "airy primal rune", "earthen primal rune", "ashen primal rune", "airy war rune",
+	private final String[] arrSkillsMidName = { "airy chaos rune", "earthen war rune", "airy primal rune", "earthen primal rune", "ashen primal rune", "airy war rune",
 					"heated battle jewel", "blighted primal rune", "icy chaos rune", "dusty battle jewel", "fiery war rune", "lightning charged war rune",
 					"unholy primal rune", "icy war rune", "watery chaos rune", "earthen chaos rune", "valiant primal rune", "vapor battle jewel",
 					"heated chaos rune", "fiery battle jewel", "heated war rune", "airy battle jewel", "fiery primal rune", "fiery chaos rune",
 					"vapor chaos rune", "dusty chaos rune", "watery war rune", "vapor war rune", "finesse war rune", "finesse primal rune" };
 
-	String[] arrSkillsHibName =
+	private final String[] arrSkillsHibName =
 					{ "steaming nature spell stone", "water war spell stone", "fiery war spell stone", "icy war spell stone", "earthen war spell stone",
 									"heated battle jewel", "oozing nature spell stone", "aberrant arcane spell stone", "vapor arcane spell stone",
 									"dusty battle jewel", "ethereal arcane spell stone", "heated war spell stone", "fiery arcane spell stone",
@@ -44,29 +44,29 @@ public class BarService {
 									"airy arcane spell stone", "mineral encrusted nature spell stone", "icy arcane spell stone", "finesse war spell stone",
 									"finesse nature spell stone" };
 
-	String[] arrFocusHibName =
+	private final String[] arrFocusHibName =
 					{ "steaming focus stone", "oozing focus stone", "vapor focus stone", "ethereal focus stone", "fiery focus stone", "watery focus stone",
 									"earthen focus stone", "phantasmal focus stone", "spectral arcane focus stone", "mineral encrusted focus stone",
 									"icy focus stone", "brilliant focus stone" };
 
-	String[] arrFocusMidName = { "ashen focus rune", "icy focus rune", "heated focus rune", "vapor focus rune", "dusty focus rune", "brilliant focus rune" };
+	private final String[] arrFocusMidName = { "ashen focus rune", "icy focus rune", "heated focus rune", "vapor focus rune", "dusty focus rune", "brilliant focus rune" };
 
-	String[] arrFocusAlbName =
+	private final String[] arrFocusAlbName =
 					{ "heated focus sigil", "icy focus sigil", "ashen focus sigil", "vacuous focus sigil", "earthen focus sigil", "fiery focus sigil",
 									"dusty focus sigil", "watery focus sigil", "salt encrusted focus sigil", "vapor focus sigil", "airy focus sigil",
 									"brilliant focus sigil" };
 
-	String[] arrResistName = { "dusty shielding jewel", "icy shielding jewel", "heated shielding jewel", "light shielding jewel", "earthen shielding jewel",
+	private final String[] arrResistName = { "dusty shielding jewel", "icy shielding jewel", "heated shielding jewel", "light shielding jewel", "earthen shielding jewel",
 					"vapor shielding jewel", "airy shielding jewel", "fiery shielding jewel", "watery shielding jewel" };
 
-	String[] arrStatsName = { "fiery essence jewel", "vapor essence jewel", "airy essence jewel", "earthen essence jewel", "dusty essence jewel",
+	private final String[] arrStatsName = { "fiery essence jewel", "vapor essence jewel", "airy essence jewel", "earthen essence jewel", "dusty essence jewel",
 					"watery essence jewel", "heated essence jewel", "icy essence jewel" };
 
-	String[] arrGemStrength = {
+	private final String[] arrGemStrength = {
 					"Raw", "Uncut", "Rough", "Flawed", "Imperfect", "Polished", "Faceted", "Precious", "Flawless", "Perfect"
 	};
 
-	String[] arrBloodEssence = { "Blood Essence Jewel" };
+	private final String[] arrBloodEssence = { "Blood Essence Jewel" };
 
 	@ConfigProperty ( name = "bar.path" )
 	String barPath;
