799afbaf6b03cd1d700a60034c1580ea7288a38a
Simone Celia <simone.celia@simonecelia.it>
2024-12-24 19:02:57 +0100

    added defaults chars and less strincts calls

--- src/main/java/it/simonecelia/edenSCputitinbarsBE/service/BarService.java
+++ src/main/java/it/simonecelia/edenSCputitinbarsBE/service/BarService.java
@@ -22,7 +22,16 @@ public class BarService {
 	@ConfigProperty ( name = "bar.path" )
 	String barPath;
 
-	public void newBar ( Realm realm, String character, int gemsNumber, String payload ) throws IOException {
+	@ConfigProperty ( name = "default.alb.crafter" )
+	String defaultAlbCrafter;
+
+	@ConfigProperty ( name = "default.hib.crafter" )
+	String defaultHibCrafter;
+
+	@ConfigProperty ( name = "default.mid.crafter" )
+	String defaultMidCrafter;
+
+	public void newBar ( Realm realm, String character, Integer gemsNumber, String payload ) throws IOException {
 		var gems = gemService.getGems ( realm, gemsNumber, payload );
 		if ( gems.isEmpty () ) {
 			throw new RuntimeException ( "Gems not found!" );
@@ -31,6 +40,19 @@ public class BarService {
 	}
 
 	private void writeBar ( List<Gem> gems, String character ) throws IOException {
+		if ( null == character ) {
+			switch ( gems.getFirst ().getRealm () ) {
+			case ALBION:
+				character = defaultAlbCrafter;
+				break;
+			case HIBERNIA:
+				character = defaultHibCrafter;
+				break;
+			case MIDGARD:
+				character = defaultMidCrafter;
+				break;
+			}
+		}
 		var iniFile = new File ( barPath + character + "-41.ini" );
 		var ini = new Wini ( iniFile );
 
